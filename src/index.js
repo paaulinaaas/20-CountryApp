22773d20b87431e91c24bb7eb1c6692fa8bcea31
Paulina Sikora <paaulinaaas@gmail.com>
2017-10-24 21:55:28 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import routes from './routes';
 
 render(
     <Provider store={store}>
-        <h1>Inicjalizacja projektu</h1>
+        <Router history={hashHistory} routes={routes}/>
     </Provider>,
     document.getElementById('root')
 );
