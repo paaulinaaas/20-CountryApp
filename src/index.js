22925de8da51bd5f11480d185e57f76b527715e7
Michał Gacek <gacek.michael@outlook.com>
2017-10-29 19:10:29 +0100

    Fix

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import App from './App';
+import { getCountries } from './actions/actions-countries';
 
 render(
     <Provider store={store}>
-        <h1>Inicjalizacja projektu</h1>
+        <App />
     </Provider>,
     document.getElementById('root')
 );
+
+store.dispatch(getCountries());
+console.log(store.getState());
